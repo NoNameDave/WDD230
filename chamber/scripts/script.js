d3a7730a9b42f9b102813131b032d29d62757475
NoNameDave <chis.wol@gmail.com>
2023-11-18 11:06:08 +0200

    Added discover.html

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -1,37 +1,64 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var darkModeToggle = document.querySelector('.dark-mode-toggle');
-    var elementsToToggle = [
-        document.querySelector('*'),
-        document.querySelector('header'),
-        document.querySelector('footer'),
-        document.querySelector('.main')
-    ];
-    
-    darkModeToggle.addEventListener('click', function() {
-        elementsToToggle.forEach(function(element) {
-            if(element) { // Check if the element was found
-                element.classList.toggle('dark-mode');
-            }
-        });
-    
-        // Update the toggle button text or attribute as needed
-        var isDarkMode = document.body.classList.contains('dark-mode');
-        darkModeToggle.textContent = isDarkMode ? 'Light Mode' : 'Dark Mode';
-    });
+  var darkModeToggle = document.querySelector('.dark-mode-toggle');
+  var elementsToToggle = [
+      document.querySelector('*'),
+      document.querySelector('header'),
+      document.querySelector('footer'),
+      document.querySelector('.main')
+  ];
   
-    // Optional: Automatically apply dark mode if user prefers it
-    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-      toggleDarkMode();
-    }
-
-    var hamburger = document.querySelector('.hamburger');
-    var navigation = document.getElementById('navigation');
+  darkModeToggle.addEventListener('click', function() {
+      elementsToToggle.forEach(function(element) {
+          if(element) { // Check if the element was found
+              element.classList.toggle('dark-mode');
+          }
+      });
   
-    hamburger.addEventListener('click', function () {
-      navigation.classList.toggle('show-nav');
-  
-      // Change the hamburger icon to 'X' when the menu is open
-      hamburger.textContent = navigation.classList.contains('show-nav') ? '✕' : '☰';
-    });
+      // Update the toggle button text or attribute as needed
+      var isDarkMode = document.body.classList.contains('dark-mode');
+      darkModeToggle.textContent = isDarkMode ? 'Light Mode' : 'Dark Mode';
   });
-  
+
+  // Optional: Automatically apply dark mode if user prefers it
+  if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    toggleDarkMode();
+  }
+
+  var hamburger = document.querySelector('.hamburger');
+  var navigation = document.getElementById('navigation');
+
+  hamburger.addEventListener('click', function () {
+    navigation.classList.toggle('show-nav');
+
+    // Change the hamburger icon to 'X' when the menu is open
+    hamburger.textContent = navigation.classList.contains('show-nav') ? '✕' : '☰';
+  });
+});
+
+// Lazy loading images
+document.addEventListener("DOMContentLoaded", function() {
+  var lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
+
+  if ("IntersectionObserver" in window) {
+      let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
+          entries.forEach(function(entry) {
+              if (entry.isIntersecting) {
+                  let lazyImage = entry.target;
+                  lazyImage.src = lazyImage.dataset.src;
+                  lazyImage.classList.remove("lazy");
+                  lazyImageObserver.unobserve(lazyImage);
+              }
+          });
+      });
+
+      lazyImages.forEach(function(lazyImage) {
+          lazyImageObserver.observe(lazyImage);
+      });
+  } else {
+      // Fallback for browsers without IntersectionObserver support
+      lazyImages.forEach(function(lazyImage) {
+          lazyImage.src = lazyImage.dataset.src;
+          lazyImage.classList.remove("lazy");
+      });
+  }
+});
--- chamber/scripts/visitCounter.js
+++ chamber/scripts/visitCounter.js
@@ -23,3 +23,24 @@ window.onload = function() {
         visitCountElement.textContent = `This page has been visited ${localStorage.getItem('visits')} times`;
     }
 };
+
+document.addEventListener("DOMContentLoaded", function() {
+    const visitMessageElement = document.getElementById('visit-message');
+    const lastVisit = localStorage.getItem('lastVisit');
+    const now = Date.now();
+    let message;
+
+    if (lastVisit) {
+        const daysSinceLastVisit = Math.floor((now - lastVisit) / (1000 * 60 * 60 * 24));
+        if (daysSinceLastVisit < 1) {
+            message = "Back so soon! Awesome!";
+        } else {
+            message = `You last visited ${daysSinceLastVisit} day${daysSinceLastVisit > 1 ? 's' : ''} ago.`;
+        }
+    } else {
+        message = "Welcome! Let us know if you have any questions.";
+    }
+
+    visitMessageElement.textContent = message;
+    localStorage.setItem('lastVisit', now);
+});
